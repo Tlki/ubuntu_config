6e4dac8c27972b75429b4cc8eb15951927df0546
Tlki <ignaciogoiteac@gmail.com>
2022-08-13 19:17:45 -0300

    [FIX] start.sh

--- start.sh
+++ start.sh
@@ -1,41 +1,35 @@
 echo "Installing dependencies"
-apt-get update
-apt-get -y install make libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
-apt-get -y install build-essential
-apt-get -y install git
-apt-get -y install curl
-apt-get -y install xsel
-apt-get -y install python3-pip
+sudo apt-get update
+sudo apt-get -y install make libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev build-essential git curl xsel python3-pip
 python3 -m pip install --user pipx
 python3 -m pipx ensurepath
 python3 -m pip install flake8 # this is mainly for ALE (vim plugin)
 
 echo "Installing vim..."
-apt-get -y install vim
-apt-get -y install vim-gtk3
+sudo apt-get -y install vim vim-gtk3
 
 echo "Installing zsh"
-apt-get -y install zsh
+sudo apt-get -y install zsh
 yes | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 chsh -s $(which zsh) $USER
 
 echo "Installing tmux..."
-apt-get -y install tmux
+sudo apt-get -y install tmux
 git clone https://github.com/tmux-plugins/tpm.git ~/.tmux/plugins/tpm
 
 echo "Installing powerline..."
 sudo apt-get -y install powerline
 
 echo "Moving zshrc to $HOME as .zshrc"
-copy zshrc ~/.zshrc
+cp zshrc ~/.zshrc
 echo "Moving tmux.conf to $HOME as .tmux.conf"
-copy tmux.conf ~/.tmux.conf
+cp tmux.conf ~/.tmux.conf
 
 # Cloning vundle plugin manager for vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 echo "Moving vimrc to $HOME as .vimrc"
-copy vimrc ~/.vimrc
+cp vimrc ~/.vimrc
 
 echo "Installing vim plugins"
 vim +PluginInstall +qall
